f91d1e2c8886ddc429da8f7bc043121d8cf9ec61
Kota Mizushima <mizukota@gmail.com>
2017-04-07 12:21:45 +0900

    Add

--- src/main/scala/com/github/scaruby/scaruby/package.scala
+++ src/main/scala/com/github/scaruby/scaruby/package.scala
@@ -1,5 +1,12 @@
 package com.github.scaruby
+import scala.language.reflectiveCalls
 
 package object scaruby {
-
+  def using[A <: { def close(): Unit }, B](resource: A)(block: A => B): B = {
+    try {
+      block(resource)
+    } finally {
+      resource.close()
+    }
+  }
 }
