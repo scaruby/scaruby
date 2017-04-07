77055950fb9b96a0718d3f3ee3bdb679cf24aaca
Kota Mizushima <mizukota@gmail.com>
2017-04-08 01:21:08 +0900

    Rename from closeWith to use.

--- src/main/scala/com/github/scaruby/scaruby/SClosableResource.scala
+++ src/main/scala/com/github/scaruby/scaruby/SClosableResource.scala
@@ -2,7 +2,7 @@ package com.github.scaruby.scaruby
 
 trait SClosableResource[A <: { def close(): Unit}] {
   def self: A
-  def closeWith[B](block: A => B): B = using(self){resource =>
+  def use[B](block: A => B): B = using(self){ resource =>
     block(resource)
   }
 }
