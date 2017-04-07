d2523e1fcea523fa38ea47491dedac19ae58bc9d
Kota Mizushima <mizukota@gmail.com>
2017-04-07 20:48:27 +0900

    Define InStream, OutStream.

---
+++ src/main/scala/com/github/scaruby/scaruby/InStream.scala
@@ -0,0 +1,36 @@
+package com.github.scaruby.scaruby
+
+import java.io.{BufferedInputStream, FileInputStream}
+
+import scala.collection.mutable
+
+class InStream(path: String) {
+  private[this] val fileStream: BufferedInputStream = new BufferedInputStream(new FileInputStream(path))
+
+  def reset(): Unit = fileStream.reset()
+
+  def mark(readlimit: Int): Unit = fileStream.mark(readlimit)
+
+  def skip(n: Long): Long = fileStream.skip(n)
+
+  def read(b: Array[Byte], off: Int, len: Int): Int = fileStream.read(b, off, len)
+
+  def markSupported(): Boolean = fileStream.markSupported()
+
+  def close(): Unit = fileStream.close()
+
+  def available(): Int = fileStream.available()
+
+  def read(): Int = fileStream.read()
+
+  def read(b: Array[Byte]): Int = fileStream.read(b)
+
+  def readAll(): Array[Byte] = {
+    val buffer = mutable.Buffer[Byte]()
+    var b: Int = -1
+    while({ b = read(); b} != -1) {
+      buffer += b.asInstanceOf[Byte]
+    }
+    buffer.toArray
+  }
+}
---
+++ src/main/scala/com/github/scaruby/scaruby/OutStream.scala
@@ -0,0 +1,21 @@
+package com.github.scaruby.scaruby
+
+import java.io.{BufferedOutputStream, FileOutputStream}
+
+class OutStream(path: String) {
+  private[this] val fileStream = new BufferedOutputStream(new FileOutputStream(path))
+
+  def write(b: Int): Unit = fileStream.write(b)
+
+  def write(b: Array[Byte], off: Int, len: Int): Unit = fileStream.write(b, off, len)
+
+  def flush(): Unit = fileStream.flush()
+
+  def write(b: Array[Byte]): Unit = fileStream.write(b)
+
+  def writeAll(s: String, encoding: String = System.getProperty("file.encoding")): Unit = {
+    write(s.getBytes(encoding))
+  }
+
+  def close(): Unit = fileStream.close()
+}
