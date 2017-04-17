0e4d06ddf166e5d3baf3b373f58442093d16eecf
Kota Mizushima <mizukota@gmail.com>
2017-04-17 19:47:06 +0900

    Renaming

--- src/main/scala/com/github/scaruby/CommandAdapter.scala
+++ src/main/scala/com/github/scaruby/CommandAdapter.scala
@@ -4,10 +4,10 @@ abstract class CommandAdapter[T] {
   def adapt(source: T): Seq[String]
 }
 object CommandAdapter {
-  implicit object PlainCommand extends CommandAdapter[String] {
+  implicit object PlainCommandAdapter extends CommandAdapter[String] {
     def adapt(source: String): Seq[String] = Seq(source)
   }
-  implicit object MultiParameterCommand extends CommandAdapter[Seq[String]] {
+  implicit object MultiParameterCommandAdapter extends CommandAdapter[Seq[String]] {
     def adapt(source: Seq[String]): Seq[String] = source
   }
 }
