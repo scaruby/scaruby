89120b1b07516db9170c7182892f6bf9776990ce
Kota Mizushima <mizukota@gmail.com>
2017-05-18 09:13:48 +0900

    Create SURLInputStream internally

--- src/main/scala/com/github/scaruby/SURL.scala
+++ src/main/scala/com/github/scaruby/SURL.scala
@@ -1,8 +1,21 @@
 package com.github.scaruby
 
+import java.io.InputStream
 import java.net.URL
 
 class SURL private (val jURL: URL) {
+  private class SURLInputStream(inputStream: InputStream) extends SInputStream[SURLInputStream] {
+
+    override def readInto(buffer: Array[Byte])(implicit offset: Int = 0, length: Int = buffer.length): Int = inputStream.read(buffer, offset, length)
+
+    override def read(): Int = inputStream.read()
+
+    override def close(): Unit = inputStream.close()
+
+    override def available(): Int = inputStream.available()
+
+    override def self: SURLInputStream = this
+  }
 }
 
 object SURL {
